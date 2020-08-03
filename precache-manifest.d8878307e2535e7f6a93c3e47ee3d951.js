0dd13f4dba5919d172e27ed938e9e57b98d7bb0b
Abhijit kumar <34920928+infinity-web@users.noreply.github.com>
2020-08-03 09:06:25 +0530

    uploading to gh pages

---
+++ precache-manifest.d8878307e2535e7f6a93c3e47ee3d951.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ced2ab0d006cef67955c9c48fd98eee5",
+    "url": "/pics/index.html"
+  },
+  {
+    "revision": "6dff53eb20874d86cb3c",
+    "url": "/pics/static/css/main.d7c154a3.chunk.css"
+  },
+  {
+    "revision": "e33d29e69669432efcf3",
+    "url": "/pics/static/js/2.58488f5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pics/static/js/2.58488f5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dff53eb20874d86cb3c",
+    "url": "/pics/static/js/main.77d813d5.chunk.js"
+  },
+  {
+    "revision": "24445b424d0ed337bcc7",
+    "url": "/pics/static/js/runtime-main.db0de8af.js"
+  }
+]);
